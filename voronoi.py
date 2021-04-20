58f8b69b633d45b684437def4a868960fe71acd2
xiaoxiae <tomas@slama.dev>
2021-04-21 00:31:44 +0200

    n^2-ish algorithm for calculating the areas

--- voronoi.py
+++ voronoi.py
@@ -1,11 +1,18 @@
 from PIL import Image, ImageColor, ImageDraw
 from random import randint, choice, seed as randomseed
 from typing import *
-from math import hypot, sqrt
+from math import hypot, sqrt, atan2
 from queue import Queue
 from dataclasses import dataclass
 from enum import Enum
+from shapely import geometry
+import svgwrite
 import os
+import shapely
+
+class OutputFormat(Enum):
+    PNG = 1
+    SVG = 2
 
 
 class ColorAlgorithm(Enum):
@@ -212,6 +219,7 @@ def generate(
         border_color = "#FFFFFF",
         animate_fill = False,
         animation_background = "#FFFFFF",
+        output_format = OutputFormat.PNG,
 ):
     # possibly seed the random algorithm
     if seed is not None:
@@ -230,68 +238,139 @@ def generate(
         Utilities.info("Calculating region centers.")
         region_centers = region_algorithm(width, height, regions)
 
-    image = [[None] * height for _ in range(width)]
-    Utilities.info("Calculating region areas.")
-    distance_algorithm(width, height, region_centers, image, float("inf"))
+    if output_format == OutputFormat.SVG:
+        svg_document = svgwrite.Drawing(filename = path + ".svg", size = (f"{width}px", f"{height}px"))
 
-    # possibly convert string colors to tuples
-    i = 0
-    while i < len(colors):
-        if type(colors[i]) == str:
-            colors[i] = Utilities.hex_to_tuple(colors[i])
+        # for each region, calculate its polygon
+        for p1 in region_centers:
+            # take each line and create a half plane
+            polygon = geometry.Polygon([(0, 0), (width, 0), (width, height), (0, height)])
+            for p2 in region_centers:
+                if p1 is p2:
+                    continue
 
-        i += 1
+                corners = [(0, 0), (width, 0), (width, height), (0, height)]
 
-    # either assign colors randomly, or calculate the chromatic number and assign them then
-    if color_algorithm == ColorAlgorithm.random:
-        Utilities.info("Assigning region colors.")
-        region_colors = {id(region): choice(colors) for region in region_centers}
-    else:
-        Utilities.info("Assigning region colors such that no two adjacent regions have the same color.")
-        region_colors = Utilities.get_different_adjacent_colors(width, height, image, colors, color_algorithm)
+                x1, y1 = p1
+                x2, y2 = p2
 
-    # the original, full image (without borders)
-    pil_image = Image.new("RGB", (width, height))
-    for x in range(width):
-        for y in range(height):
-            pil_image.putpixel((x, y), region_colors[image[x][y]])
+                # the line is vertical
+                if (y1 == y2):
+                    p = [(x1, y1), (x1, y2)]
 
-    if border_size != 0:
-        Utilities.add_border(border_color, border_size, image, pil_image, width, height)
+                    a = None
 
-    if animate_fill:
-        if not os.path.exists(path):
-            os.makedirs(path)
+                # the line is not vertical
+                else:
+                    # TODO: other metrics!
+                    a = - (x2 - x1) / (y2 - y1)
+                    b = (y1 + y2) / 2 - a * (x1 + x2) / 2
 
-        d = 1
+                    f = lambda x: a * x + b
 
-        if type(animation_background) == str:
-            animation_background = Utilities.hex_to_tuple(animation_background)
+                    L = (0, f(0))
+                    U = (-b / a, 0)
+                    R = (width, f(width))
+                    D = ((height - b) / a, height)
 
-        while True:
-            animation_image = [[None] * height for _ in range(width)]
-            distance_algorithm(width, height, region_centers, animation_image, d)
+                    p = []
 
-            animation_pil_image = Image.new("RGB", (width, height))
+                    if 0 <= L[1] <= height:
+                        p.append(L)
+                    if 0 <= R[1] <= height:
+                        p.append(R)
+                    if 0 <= D[0] <= width:
+                        p.append(D)
+                    if 0 <= U[0] <= width:
+                        p.append(U)
 
-            for x in range(width):
-                for y in range(height):
-                    animation_pil_image.putpixel((x, y), animation_background if animation_image[x][y] is None else region_colors[image[x][y]])
+                side = lambda x, y: (a is None and x < x1) or ((f(x) < y) == (f(x1) < y1))
 
-            if border_size != 0:
-                Utilities.add_border(border_color, border_size, animation_image, animation_pil_image, width, height)
+                p += [(x, y) for x, y in corners if side(x, y)]
 
-            animation_path = os.path.join(path, f"{d}.png")
+                if len(p) <= 2:
+                    continue
 
-            animation_pil_image.save(animation_path, "PNG")
-            Utilities.success(f"Animation image saved to {animation_path}")
+                # sort them by planar coordinates form the region
+                # the polygon is convex so there is no problem with this
+                p = sorted(p, key = lambda p: atan2(p1[0] - p[0], p1[1] - p[1]))
+                polygon = polygon.intersection(geometry.Polygon(p))
 
-            d += 1
+            # TODO: colors!
+            svg_document.add(svgwrite.shapes.Polygon(polygon.exterior.coords[:-1], fill=("rgb" + str(region_colors[p1]))))
 
-            if image == animation_image:
-                Utilities.success(f"Done!")
-                break
+        svg_document.save()
 
-    else:
-        pil_image.save(path, "PNG")
-        Utilities.success(f"Image saved to {path}!")
+    if output_format == OutputFormat.PNG:
+        image = [[None] * height for _ in range(width)]
+        Utilities.info("Calculating region areas.")
+        distance_algorithm(width, height, region_centers, image, float("inf"))
+
+        # possibly convert string colors to tuples
+        i = 0
+        while i < len(colors):
+            if type(colors[i]) == str:
+                colors[i] = Utilities.hex_to_tuple(colors[i])
+
+            i += 1
+
+        # either assign colors randomly, or calculate the chromatic number and assign them then
+        if color_algorithm == ColorAlgorithm.random:
+            Utilities.info("Assigning region colors.")
+            region_colors = {id(region): choice(colors) for region in region_centers}
+        else:
+            Utilities.info("Assigning region colors such that no two adjacent regions have the same color.")
+            region_colors = Utilities.get_different_adjacent_colors(width, height, image, colors, color_algorithm)
+
+        # the original, full image (without borders)
+        pil_image = Image.new("RGB", (width, height))
+        for x in range(width):
+            for y in range(height):
+                pil_image.putpixel((x, y), region_colors[image[x][y]])
+
+        if border_size != 0:
+            Utilities.add_border(border_color, border_size, image, pil_image, width, height)
+
+        if animate_fill:
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            d = 1
+
+            if type(animation_background) == str:
+                animation_background = Utilities.hex_to_tuple(animation_background)
+
+            while True:
+                animation_image = [[None] * height for _ in range(width)]
+                distance_algorithm(width, height, region_centers, animation_image, d)
+
+                animation_pil_image = Image.new("RGB", (width, height))
+
+                for x in range(width):
+                    for y in range(height):
+                        animation_pil_image.putpixel((x, y), animation_background if animation_image[x][y] is None else region_colors[image[x][y]])
+
+                if border_size != 0:
+                    Utilities.add_border(border_color, border_size, animation_image, animation_pil_image, width, height)
+
+                animation_path = os.path.join(path, f"{d}.png")
+
+                animation_pil_image.save(animation_path, "PNG")
+                Utilities.success(f"Animation image saved to {animation_path}")
+
+                d += 1
+
+                if image == animation_image:
+                    Utilities.success(f"Done!")
+                    break
+
+        else:
+            pil_image.save(path, "PNG")
+            Utilities.success(f"Image saved to {path}!")
+
+generate(
+    path = "1",
+    regions = 8,
+    colors = [(0, 0, 0), (15, 15, 15), (23, 23, 23), (30, 30, 30)],
+    output_format = OutputFormat.SVG,
+)
